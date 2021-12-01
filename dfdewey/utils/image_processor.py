4bc951a06104bbffe08f00c877068902648d7d66
Jason <52063018+dfjxs@users.noreply.github.com>
2021-12-01 11:32:31 +1100

    Fix for getting volume offset (#25)
    * Fix logic for extracting volume offset

--- dfdewey/utils/image_processor.py
+++ dfdewey/utils/image_processor.py
@@ -24,6 +24,8 @@ from dfvfs.helpers import volume_scanner
 from dfvfs.lib import definitions as dfvfs_definitions
 from dfvfs.lib import errors as dfvfs_errors
 from dfvfs.resolver import resolver
+from dfvfs.volume import gpt_volume_system
+from dfvfs.volume import lvm_volume_system
 from dfvfs.volume import tsk_volume_system
 import pytsk3
 
@@ -119,24 +121,6 @@ class FileEntryScanner(volume_scanner.VolumeScanner):
       inode = getattr(path_spec, 'inode', None)
     return inode
 
-  def _get_tsk_partition_path_spec(self, path_spec):
-    """Gets the path spec for the TSK partition.
-
-    Args:
-      path_spec (dfvfs.PathSpec): path spec of the volume.
-
-    Returns:
-      TSK partition path_spec or None.
-    """
-    partition_path_spec = None
-    while path_spec.HasParent():
-      type_indicator = path_spec.type_indicator
-      if type_indicator == dfvfs_definitions.TYPE_INDICATOR_TSK_PARTITION:
-        partition_path_spec = path_spec
-        break
-      path_spec = path_spec.parent
-    return partition_path_spec
-
   def _get_volume_location(self, path_spec):
     """Gets volume location / identifier for the given path spec.
 
@@ -213,30 +197,43 @@ class FileEntryScanner(volume_scanner.VolumeScanner):
       base_path_specs = self.GetBasePathSpecs(image_path, options=options)
 
       for path_spec in base_path_specs:
-        partition_path_spec = self._get_tsk_partition_path_spec(path_spec)
-        if not partition_path_spec:
-          location = getattr(path_spec, 'location', None)
-          self._volumes[location] = {'start': 0, 'end': None}
-        else:
-          location = getattr(partition_path_spec, 'location', None)
-          partition_offset = None
-          partition_size = None
+        partition_offset = None
+        partition_size = None
+        partition_location = None
+        fs_location = getattr(path_spec, 'location', None)
+        while path_spec.HasParent():
+          type_indicator = path_spec.type_indicator
+          if type_indicator in (dfvfs_definitions.TYPE_INDICATOR_GPT,
+                                dfvfs_definitions.TYPE_INDICATOR_LVM,
+                                dfvfs_definitions.TYPE_INDICATOR_TSK_PARTITION):
+            if fs_location in ('\\', '/'):
+              fs_location = getattr(path_spec, 'location', None)
+            partition_location = getattr(path_spec, 'location', None)
+            if type_indicator == dfvfs_definitions.TYPE_INDICATOR_TSK_PARTITION:
+              volume_system = tsk_volume_system.TSKVolumeSystem()
+            elif type_indicator == dfvfs_definitions.TYPE_INDICATOR_LVM:
+              volume_system = lvm_volume_system.LVMVolumeSystem()
+            else:
+              volume_system = gpt_volume_system.GPTVolumeSystem()
+            try:
+              volume_system.Open(path_spec)
+              volume_identifier = partition_location.replace('/', '')
+              volume = volume_system.GetVolumeByIdentifier(volume_identifier)
+              partition_offset = volume.extents[0].offset
+              partition_size = volume.extents[0].size
+            except dfvfs_errors.VolumeSystemError as e:
+              log.error('Could not process partition: %s', e)
+            break
+          path_spec = path_spec.parent
 
-          volume_system = tsk_volume_system.TSKVolumeSystem()
-          try:
-            volume_system.Open(partition_path_spec)
-            volume_identifier = location.replace('/', '')
-            volume = volume_system.GetVolumeByIdentifier(volume_identifier)
-
-            partition_offset = volume.extents[0].offset
-            partition_size = volume.extents[0].size
-          except dfvfs_errors.VolumeSystemError as e:
-            log.error('Could not process partition: %s', e)
-
-          self._volumes[location] = {
-              'start': partition_offset,
-              'end': partition_offset + partition_size
-          }
+        if not partition_location:
+          partition_location = fs_location
+          partition_offset = 0
+          partition_size = 0
+        self._volumes[partition_location] = {
+            'start': partition_offset,
+            'end': partition_offset + partition_size
+        }
 
     return self._volumes
 
@@ -384,17 +381,34 @@ class ImageProcessor():
     Returns:
       Volume location / identifier and byte offset.
     """
-    location = getattr(path_spec, 'location', None)
-    start_offset = 0
+    fs_location = getattr(path_spec, 'location', None)
     while path_spec.HasParent():
       type_indicator = path_spec.type_indicator
-      if type_indicator == dfvfs_definitions.TYPE_INDICATOR_TSK_PARTITION:
-        if location in ('\\', '/'):
-          location = getattr(path_spec, 'location', None)
-        start_offset = getattr(path_spec, 'start_offset', 0)
+      if type_indicator in (dfvfs_definitions.TYPE_INDICATOR_GPT,
+                            dfvfs_definitions.TYPE_INDICATOR_LVM,
+                            dfvfs_definitions.TYPE_INDICATOR_TSK_PARTITION):
+        if fs_location in ('\\', '/'):
+          fs_location = getattr(path_spec, 'location', None)
+        partition_location = getattr(path_spec, 'location', None)
+
+        if type_indicator == dfvfs_definitions.TYPE_INDICATOR_TSK_PARTITION:
+          volume_system = tsk_volume_system.TSKVolumeSystem()
+        elif type_indicator == dfvfs_definitions.TYPE_INDICATOR_LVM:
+          volume_system = lvm_volume_system.LVMVolumeSystem()
+        else:
+          volume_system = gpt_volume_system.GPTVolumeSystem()
+
+        try:
+          volume_system.Open(path_spec)
+          volume_identifier = partition_location.replace('/', '')
+          volume = volume_system.GetVolumeByIdentifier(volume_identifier)
+          partition_offset = volume.extents[0].offset
+        except dfvfs_errors.VolumeSystemError as e:
+          raise RuntimeError('Unable to get volume details.') from e
         break
       path_spec = path_spec.parent
-    return location, start_offset
+
+    return partition_location, partition_offset
 
   def _index_record(self, index_name, string_record):
     """Index a single record.
